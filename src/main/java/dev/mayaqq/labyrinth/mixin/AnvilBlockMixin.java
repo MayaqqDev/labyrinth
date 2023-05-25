94a824979f6122034fa63b61dbe6c9f0766192ab
Maximusbarcz <maxim.baranek@gmail.com>
2023-05-25 20:39:47 +0200

    some small changes

--- src/main/java/dev/mayaqq/labyrinth/mixin/AnvilBlockMixin.java
+++ src/main/java/dev/mayaqq/labyrinth/mixin/AnvilBlockMixin.java
@@ -6,6 +6,7 @@ import dev.mayaqq.labyrinth.utils.mutliblock.Multiblock;
 import net.minecraft.block.AnvilBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.predicate.block.BlockStatePredicate;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -27,26 +28,27 @@ import java.util.function.Predicate;
 @Mixin(AnvilBlock.class)
 public class AnvilBlockMixin {
     private static char[][][] COMPLETED_FORGE;
+    private static final HashMap<BlockPos, Multiblock> forgeMultiblocks = new HashMap<>();
     @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
     private void labyrinth$onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-        HashMap<Character, Predicate<BlockState>> predicates = new HashMap<>();
-        predicates.put('a', BlockStatePredicate.ANY);
-        predicates.put('$', BlockStatePredicate.forBlock(Blocks.ANVIL));
-        predicates.put('b', blockState -> blockState.isIn(TagRegistry.FORGE_BRICKS));
-        predicates.put('w', blockState -> blockState.isIn(TagRegistry.FORGE_WALLS));
-        predicates.put('x', blockState -> blockState.isIn(TagRegistry.FORGE_BASE));
-        predicates.put('s', blockState -> blockState.isIn(TagRegistry.FORGE_STAIRS));
-        predicates.put('c', BlockStatePredicate.forBlock(Blocks.CHAIN));
-        Multiblock forgeMultiblock = new Multiblock(this.getForge(), predicates);
         if (world.isClient) {
             cir.setReturnValue(ActionResult.SUCCESS);
         } else {
+            getForgeMultiblock(pos);
             // get the block below the anvil
             BlockPos blockPos = pos.down();
-            if (world.getBlockState(blockPos).isIn(TagRegistry.FORGE_BASE) && forgeMultiblock.check(pos, world)) {
-                ForgeGui.gui((ServerPlayerEntity) player, pos);
-            } else if (world.getBlockState(blockPos).isIn(TagRegistry.FORGE_BASE)) {
-                player.sendMessage(Text.translatable("gui.labyrinth.forge.message.negative.forge"), true);
+            if (world.getBlockState(blockPos).isIn(TagRegistry.FORGE_BASE)) {
+                Multiblock forge = forgeMultiblocks.get(pos);
+                if (player.isInPose(EntityPose.CROUCHING)) {
+                    forge.rotate();
+                    if (forge.getPreviewed()) {
+                        forge.check(pos, world);
+                    }
+                } else if (forge.check(pos, world)) {
+                    ForgeGui.gui((ServerPlayerEntity) player, pos);
+                } else {
+                    player.sendMessage(Text.translatable("gui.labyrinth.forge.message.negative.forge"), true);
+                }
             } else {
                 player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
             }
@@ -63,8 +65,25 @@ public class AnvilBlockMixin {
                     {{'w', 'a', 'a', 'a', 'w'}, {'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a'}, {'w', 'a', 'a', 'a', 'w'}},
                     {{'w', 'a', 'a', 'a', 'w'}, {'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'w', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a'}, {'w', 'a', 'a', 'a', 'w'}},
                     {{'b', 'a', 'a', 'a', 'b'}, {'a', 'a', 'a', 'a', 'a'}, {'a', 'a', 'c', 'a', 'a'}, {'a', 'a', 'a', 'a', 'a'}, {'b', 'a', 'a', 'a', 'b'}},
-                    {{'a', 'b', 'b', 'b', 'a'}, {'b', 'b', 'b', 'b', 'b'}, {'b', 'b', 'b', 'b', 'b'}, {'b', 'b', 'b', 'b', 'b'}, {'a', 'b', 'b', 'b', 'a'},}};
+                    {{'a', 'b', 'b', 'b', 'a'}, {'b', 'b', 'b', 'b', 'b'}, {'b', 'b', 'b', 'b', 'b'}, {'b', 'b', 'b', 'b', 'b'}, {'a', 'b', 'b', 'b', 'a'}}
+            };
         }
         return COMPLETED_FORGE;
+    }
+    private  static HashMap<Character, Predicate<BlockState>> getPredicates() {
+        HashMap<Character, Predicate<BlockState>> predicates = new HashMap<>();
+        predicates.put('a', BlockStatePredicate.ANY);
+        predicates.put('$', BlockStatePredicate.forBlock(Blocks.ANVIL));
+        predicates.put('b', blockState -> blockState.isIn(TagRegistry.FORGE_BRICKS));
+        predicates.put('w', blockState -> blockState.isIn(TagRegistry.FORGE_WALLS));
+        predicates.put('x', blockState -> blockState.isIn(TagRegistry.FORGE_BASE));
+        predicates.put('s', blockState -> blockState.isIn(TagRegistry.FORGE_STAIRS));
+        predicates.put('c', BlockStatePredicate.forBlock(Blocks.CHAIN));
+        return predicates;
+    }
+    private static void getForgeMultiblock(BlockPos pos) {
+        if (forgeMultiblocks.get(pos) == null) {
+            forgeMultiblocks.put(pos, new  Multiblock(getForge(), getPredicates(), true));
+        }
     }
 }
--- src/main/java/dev/mayaqq/labyrinth/utils/mutliblock/Multiblock.java
+++ src/main/java/dev/mayaqq/labyrinth/utils/mutliblock/Multiblock.java
@@ -9,7 +9,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.registry.Registries;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.joml.Vector3f;
@@ -20,21 +19,26 @@ import java.util.function.Predicate;
 import static dev.mayaqq.labyrinth.Labyrinth.LOGGER;
 
 public class Multiblock {
-    private final char[][][] pattern;
+    private char[][][] pattern;
     private final HashMap<Character, Predicate<BlockState>> predicates;
+    private final boolean shouldPreview;
     private final int width;
     private final int height;
     private final int length;
+    private boolean previewing;
+    private int previewed = 0;
 
     public static HashMap<BlockPos, HolderAttachment> attachments = new HashMap<>();
     public static HashMap<BlockPos, BlockDisplayElement> elements = new HashMap<>();
 
-    public Multiblock(char[][][] pattern, HashMap<Character, Predicate<BlockState>> predicates) {
+    public Multiblock(char[][][] pattern, HashMap<Character, Predicate<BlockState>> predicates, boolean shouldPreview) {
         this.pattern = pattern;
         this.predicates = predicates;
         this.width = pattern[0].length;
         this.height = pattern.length;
         this.length = pattern[0][0].length;
+        this.shouldPreview = shouldPreview;
+        this.previewing = false;
     }
 
     public int getWidth() {
@@ -47,6 +51,10 @@ public class Multiblock {
 
     public int getLength() {
         return this.length;
+    }
+
+    public boolean getPreviewed() {
+        return this.previewing;
     }
 
     public boolean check(BlockPos pos, World world) {
@@ -63,46 +71,49 @@ public class Multiblock {
                     BlockPos blockPos = corner.add(j, i, k);
                     Predicate<BlockState> predicate = predicates.get(pattern[i][j][k]);
                     boolean isRightBlock = predicate.test(world.getBlockState(blockPos));
-                    BlockDisplayElement element = new BlockDisplayElement();
-
                     //if the block is already in the map, remove it
-                    if (attachments.containsKey(blockPos)) {
-                        attachments.get(blockPos).destroy();
-                        attachments.remove(blockPos);
-                        elements.remove(blockPos);
-                    } else {
-                        // if the block is not in the map, add it and make cool
-                        ElementHolder holder = new ElementHolder() {
-                            @Override
-                            public Vec3d getPos() {
-                                return new Vec3d(blockPos.getX(), blockPos.getY(), blockPos.getZ());
-                            }
-                        };
-                        HolderAttachment attachment = ChunkAttachment.of(holder, (ServerWorld) world, blockPos);
-                        // TODO: optimize this
-                        for (Block state : Registries.BLOCK) {
-                            BlockState blockState = state.getDefaultState();
-                            if (predicate.test(blockState)) {
-                                element.setBlockState(blockState);
-                                break;
-                            }
-                        }
-                        // if the block is not air and is not the right block, make it glow
-                        if (!world.getBlockState(blockPos).isAir() && !isRightBlock) {
-                            element.setGlowing(true);
-                        }
-                        // put the block in the middle of the block
-                        element.setOffset(new Vec3d(0.25F, 0.25F, 0.25F));
-                        // make the scale 0 if the block is right, so its invisible, and 0.5 if its wrong so its visible
-                        if (!isRightBlock) {
-                            element.setScale(new Vector3f(0.5F, 0.5F, 0.5F));
+                    if (shouldPreview) {
+                        BlockDisplayElement element = new BlockDisplayElement();
+                        if (attachments.containsKey(blockPos)) {
+                            attachments.get(blockPos).destroy();
+                            attachments.remove(blockPos);
+                            elements.remove(blockPos);
+                            previewed = 0;
                         } else {
-                            element.setScale(new Vector3f(0.0F, 0.0F, 0.0F));
+                            previewed++;
+                            // if the block is not in the map, add it and make cool
+                            ElementHolder holder = new ElementHolder() {
+                                @Override
+                                public Vec3d getPos() {
+                                    return new Vec3d(blockPos.getX(), blockPos.getY(), blockPos.getZ());
+                                }
+                            };
+                            HolderAttachment attachment = ChunkAttachment.of(holder, (ServerWorld) world, blockPos);
+                            // TODO: optimize this
+                            for (Block state : Registries.BLOCK) {
+                                BlockState blockState = state.getDefaultState();
+                                if (predicate.test(blockState)) {
+                                    element.setBlockState(blockState);
+                                    break;
+                                }
+                            }
+                            // if the block is not air and is not the right block, make it glow
+                            if (!world.getBlockState(blockPos).isAir() && !isRightBlock) {
+                                element.setGlowing(true);
+                            }
+                            // put the block in the middle of the block
+                            element.setOffset(new Vec3d(0.25F, 0.25F, 0.25F));
+                            // make the scale 0 if the block is right, so its invisible, and 0.5 if its wrong so its visible
+                            if (!isRightBlock) {
+                                element.setScale(new Vector3f(0.5F, 0.5F, 0.5F));
+                            } else {
+                                element.setScale(new Vector3f(0.0F, 0.0F, 0.0F));
+                            }
+                            // put the block in the map
+                            holder.addElement(element);
+                            attachments.put(blockPos, attachment);
+                            elements.put(blockPos, element);
                         }
-                        // put the block in the map
-                        holder.addElement(element);
-                        attachments.put(blockPos, attachment);
-                        elements.put(blockPos, element);
                     }
                     // if the block is not the right block, return false
                     if (!isRightBlock) {
@@ -111,11 +122,30 @@ public class Multiblock {
                 }
             }
         }
+        if (previewed > 0) {
+            previewing = true;
+        } else {
+            previewing = false;
+        }
         return result;
     }
 
-    public static void rotate() {
+    public void rotate() {
+        // does not work yet
+        int height = getHeight();
+        int width = getWidth();
+        int length = getLength();
 
+        char[][][] rotated = new char[height][width][length];
+
+        for (int c = 0; c < height; c++) {
+            for (int i = 0; i < length; i++) {
+                for (int j = 0; j < width; j++) {
+                    rotated[c][i][j] = pattern[c][width - 1 - j][i];
+                }
+            }
+        }
+        pattern = rotated;
     }
 
     public BlockPos findOffset(BlockPos pos) {
